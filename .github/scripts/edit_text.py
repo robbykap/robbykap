3f4ee07e8775b7869cc17b0faac18cf50e9e321e
Robert Greathouse <robbykap@byu.edu>
2025-02-12 14:17:01 -0700

    updated

--- .github/scripts/edit_text.py
+++ .github/scripts/edit_text.py
@@ -1,5 +1,26 @@
+from argparse import ArgumentParser
+
+
+def main(message: str) -> str | None:
+    if not message:
+        return None
+
+    # Remove Items to deploy: from the message
+    message = message.replace('Items to deploy:', '')
+
+    new_message = ""
+    items = message.split('-')[1:]
+    for item in items:
+        item = item.strip()
+        item_type, item_name = item.split(' ', 1)
+        new_message += f"- {item_type.upper()} {item_name}\n"
+
+    return new_message
+
+
 if __name__ == '__main__':
     argparse = ArgumentParser()
     argparse.add_argument('--message', type=str)
     args = argparse.parse_args()
 
+    main(args.message)
