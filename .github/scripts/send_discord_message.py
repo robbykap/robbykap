793e87bf268b11568c263c9e0a7b3683059046ac
Robert Greathouse <robbykap@byu.edu>
2025-02-19 12:21:45 -0700

    Updated dont expose my discord webhook url

--- .github/scripts/send_discord_message.py
+++ .github/scripts/send_discord_message.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from datetime import datetime
 
@@ -36,55 +37,68 @@ def get_info(message: str) -> tuple[list[str], list[str]]:
     return parse_message(to_deploy, update)
 
 
-def get_fields(author: str, branch: str, deployed_files: list[str], quizzes_to_update: list[str]) -> list[dict]:
-    fields = [
-        {"name": "\u200b", "value": "\u200b", "inline": False},
-        {"name": "Author", "value": author, "inline": True},
-        {"name": "\u200b", "value": "\u200b", "inline": True},
-        {"name": "Branch", "value": branch, "inline": True},
-        {"name": "\u200b", "value": "\u200b", "inline": False},
-    ]
+def get_fields(deployed_files: list[str], quizzes_to_update: list[str]) -> list[dict]:
+    fields = [{"name": "\u200b", "value": "\u200b", "inline": False}]
 
     if deployed_files:
-        fields.append({"name": "Items to deploy", "value": '\n'.join(deployed_files), "inline": True})
+        fields.append({"name": "Item(s) deployed", "value": '\n'.join(deployed_files), "inline": True})
         fields.append({"name": "\u200b", "value": "\u200b", "inline": True})
 
     if quizzes_to_update:
         fields.append({"name": "Updated Quiz(s)", "value": '\n'.join(quizzes_to_update), "inline": True})
 
+    fields.append({"name": "\u200b", "value": "\u200b", "inline": False})
     return fields
 
 
-def send_request(description: str, author: str, branch: str, deployed_files: list[str] = None, quizzes_to_update: list[str] = None):
-    fields = get_fields(author, branch, deployed_files, quizzes_to_update)
+def send_request(
+        author: str,
+        author_icon: str,
+        branch: str,
+        deployed_files: list[str] = None,
+        quizzes_to_update: list[str] = None
+):
+    fields = get_fields(deployed_files, quizzes_to_update)
     payload = {
-        "username": "MDXCanvas GitHub Action",
+        "username": "Canvas Notifications",
         "avatar_url": "https://digitallearning.utah.edu/_resources/images/logos/canvas/canvas-bug.png",
         "embeds": [{
-            "title": "Canvas Updates",
-            "description": description,
+            "author": {
+                "name": author,
+                "icon_url": author_icon
+            },
+            "title": "CS 110 - Course Updates",
+            "description": f'**`{branch}`**'
+                           f'\n'
+                           f'\n'
+                           f'The following branch has been merged and subsequent changes have been made or need saving...',
             "color": 16711764,
             "fields": fields,
-            "timestamp": datetime.utcnow().isoformat()
+            "timestamp": datetime.utcnow().isoformat(),
+            "footer": {
+                "text": "MDXCanvas GitHub Action",
+                "icon_url": "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+            }
         }]
     }
     print(json.dumps(payload, indent=4))
-    response = requests.post("https://discord.com/api/webhooks/1339110649665425499/1ZXtpOKpCS1ExBVfcVwc3pF3Z4H_bj9LzKYW2s0Z7bsWMPwOnfOXSpVgNf-lfuaMxnng", json=payload)
+    response = requests.post(WEBOOK_URL, json=payload)
     response.raise_for_status()
 
 
-def main(message: str, description: str, author: str, branch: str):
+def main(message: str, author: str, author_icon:str, branch: str):
     deployed_files, quizzes_to_update = get_info(message)
-    send_request(description, author, branch, deployed_files, quizzes_to_update)
+    send_request(author, author_icon, branch, deployed_files, quizzes_to_update)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--message', required=True)
-    parser.add_argument('--description', required=True)
     parser.add_argument('--author', required=True)
     parser.add_argument('--author-icon', required=True)
     parser.add_argument('--branch', required=True)
     args = parser.parse_args()
 
-    main(args.message, args.description, args.authoricon, args.author, args.branch)
+    WEBOOK_URL = os.getenv('TEST_CANVAS_DISCORD_WEBHOOK')
+
+    main(args.message, args.author, args.author_icon, args.branch)
