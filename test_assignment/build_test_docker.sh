01772db6506c736ca0a5c97e404f734b604bcfe0
Robert Greathouse <robbykap@byu.edu>
2025-02-26 20:01:57 -0700

    Test edit to both at the same time

--- test_assignment/build_test_docker.sh
+++ test_assignment/build_test_docker.sh
@@ -13,7 +13,7 @@ docker buildx build \
 
 FROM byucscourseops/cs110-autograder-bit-base:winter2025-newbit
 
-ADD solutions/worlds /autograder/src/worlds
+ADD solution/worlds /autograder/src/worlds
 
 ADD activities.json /autograder/activities.json
 
